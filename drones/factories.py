280e289d779bb0975c195aa275811d18ec9c967e
Anastasiia <anastasiia.humeniuk.shi.2024@lpnu.ua>
2026-05-29 16:08:50 +0300

    Add typical performance metrics to DroneSpecFactory and related tests

--- drones/factories.py
+++ drones/factories.py
@@ -87,7 +87,9 @@ class DroneSpecFactory(factory.django.DjangoModelFactory):
     flight_controller = "Test Controller"
     firmware_version = "1.0.0"
     max_speed_kmh = "120.00"
+    typical_range_km = "8.50"
     max_range_km = "10.00"
+    typical_flight_time_min = "18.00"
     max_flight_time_min = "20.00"
     frequency_mhz = 5800
     payload_capacity_g = 100
