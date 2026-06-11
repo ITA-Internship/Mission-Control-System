075750499a77e3046ef53d527a458d934ff2c589
travis <danylo.klymyshyn.shi.2024@lpnu.ua>
2026-06-11 15:54:40 +0300

    added endpoint for drone comparison

--- drones/urls.py
+++ drones/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import DroneDetailView, DroneListCreateView
+from .views import DroneComparisonView, DroneDetailView, DroneListCreateView
 
 app_name = "drones"
 
 urlpatterns = [
     path("", DroneListCreateView.as_view(), name="drone-create"),
+    path("compare/", DroneComparisonView.as_view(), name="drone-compare"),
     path("<int:pk>/", DroneDetailView.as_view(), name="drone-detail"),
 ]
