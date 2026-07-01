69af8cf9d362036e9a71d91e7f100c4d11392464
travis <danylo.klymyshyn.shi.2024@lpnu.ua>
2026-07-01 20:43:42 +0300

    fixed celery

--- config/urls.py
+++ config/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path("api/missions/", include("missions.urls")),
     path("api/repairs/", include("repairs.urls")),
     path("api/media/", include("media.urls_video")),
+    path("api/missions/<int:mission_pk>/artifacts/", include("media.urls")),
     path("api/media/", include(media_audit_urlpatterns)),
 ]
 
