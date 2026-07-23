89ed82bdf263d87408ac6ab3d613fb3186ca9ece
Maksym Lirko <maksym.lirko@gmail.com>
2026-07-23 15:23:38 +0300

    Add function to protect from csv injections

--- common/utils.py
+++ common/utils.py
@@ -1,3 +1,20 @@
 class EchoBuffer:
     def write(self, value):
         return value
+
+
+DANGEROUS_PREFIXES = ("=", "+", "-", "@", "|")
+
+
+def sanitize_cell(value):
+    if isinstance(value, str):
+        stripped_value = value.lstrip()
+
+        if stripped_value.startswith(DANGEROUS_PREFIXES):
+            return f"'{value}"
+
+    return value
+
+
+def sanitize_row(row):
+    return [sanitize_cell(cell) for cell in row]
